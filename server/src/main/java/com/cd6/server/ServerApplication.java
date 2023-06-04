a9d28d3039dcdca26b40cfcabf26cd7823108dee
sincheol <ohmong0803@gmail.com>
2023-06-04 11:57:41 +0900

    auth-github-login

--- server/src/main/java/com/cd6/server/ServerApplication.java
+++ server/src/main/java/com/cd6/server/ServerApplication.java
@@ -1,13 +1,68 @@
 package com.cd6.server;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationEntryPointFailureHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @SpringBootApplication
-public class ServerApplication {
+@RestController
+public class ServerApplication{
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
+    @GetMapping("/user") //index.html에 user 정보를 넘겨줌
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal){
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
+
+    @GetMapping("/error")
+    public String error(HttpServletRequest request) {
+        String message = (String) request.getSession().getAttribute("error.message");
+        request.getSession().removeAttribute("error.message");
+        return message;
+    }
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+    @Configuration
+    public static class SecurityConfig{
+
+        private AuthenticationEntryPointFailureHandler handler;
+
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests(authorize -> authorize
+                            .requestMatchers("/", "/error", "/webjars/**").permitAll()
+                            .anyRequest().authenticated()
+                    )
+                    .oauth2Login(Customizer.withDefaults())
+                    .logout(l -> l
+                            .logoutSuccessUrl("/").permitAll()
+                    )
+                    .csrf(c -> c
+                            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                    );
+
+
+            return http.build();
+        }
+    }
 
 }
