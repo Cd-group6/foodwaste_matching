9f5a742282e096df5e20fe4fd62c06c60f20ba64
LKR <lkr9902@naver.com>
2023-09-09 01:11:46 +0900

    Match UI

--- client/src/pages/Match.js
+++ client/src/pages/Match.js
@@ -1,17 +1,31 @@
 import React from "react";
 import { useState,useEffect} from 'react';
 import {
+    widthPercentageToDP as wp,
+    heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import {
     StyleSheet,
     Text,
     View,
     ScrollView,
     StatusBar,
     TouchableOpacity,
+    Switch,
     } from 'react-native';
 import {theme} from "../colors.js";
 import Search from '../components/SearchBar'
 
 const Match = ({navigation}) => {
+
+    const [size, setSize] = useState(false);
+    const tSwitch1 = () => setSize(previousState => !previousState);
+
+    const [trashCan, setTrashCan] = useState(false);
+    const tSwitch2 = () => setTrashCan(previousState => !previousState);
+
+    const [agree, setAgree] = useState(false);
+    const tSwitch3 = () => setAgree(previousState => !previousState);
 
     return (
         <View style={styles.container}>
@@ -19,18 +33,53 @@ const Match = ({navigation}) => {
             <Search />
             <View style={styles.header}>
                 <TouchableOpacity onPress={() => navigation.navigate('Chat')}>
-                    <Text style={styles.textc}>채팅</Text>
+                    <Text style={styles.textC}>채팅</Text>
                 </TouchableOpacity>
 
-                <Text style={styles.textm}>매칭</Text>
-
+                <Text style={styles.textM}>매칭</Text>
             </View>
             <ScrollView showsVerticalScrollIndicator={false}>
                 <View style={styles.room}>
                     <Text style={styles.chat}> Match </Text>
                 </View>
-                <View style={styles.chat}></View>
-                <View style={styles.chat}></View>
+
+                <View style={styles.line}></View>
+
+                <View style={styles.option1}>
+                    <Text style={styles.opText}>봉투 규격(3L/5L)</Text>
+                    <Text style={styles.stText}>{size ? '3L' : '5L'}</Text>
+                    <Switch style={styles.opSwitch}
+                        trackColor={{false: '#767577', true: theme.mainC}}
+                        thumbColor={size ? theme.bg : '#f4f3f4'}
+                        ios_backgroundColor="#3e3e3e"
+                        onValueChange={tSwitch1}
+                        value={size}/>
+                </View>
+                <View style={styles.option1}>
+                    <Text style={styles.opText}>쓰레기 통</Text>
+                    <Text style={styles.stText}>{trashCan ? '보유' : '미보유'}</Text>
+                    <Switch style={styles.opSwitch}
+                        trackColor={{false: '#767577', true: theme.mainC}}
+                        thumbColor={trashCan ? theme.bg : '#f4f3f4'}
+                        ios_backgroundColor="#3e3e3e"
+                        onValueChange={tSwitch2}
+                        value={trashCan}/>
+                </View>
+                <View style={styles.option1}>
+                    <Text style={styles.opText}>보증금 활용 동의</Text>
+                    <Text style={styles.stText}>{agree ? '동의' : '미동의'}</Text>
+                    <Switch style={styles.opSwitch}
+                        trackColor={{false: '#767577', true: theme.mainC}}
+                        thumbColor={agree ? theme.bg : '#f4f3f4'}
+                        ios_backgroundColor="#3e3e3e"
+                        onValueChange={tSwitch3}
+                        value={agree}/>
+                </View>
+                <TouchableOpacity
+                    style={styles.btn}
+                    onPress={() => navigation.navigate('Chat')}>
+                    <Text style={(styles.Text, {color: 'white'})}>매칭 시작</Text>
+                </TouchableOpacity>
             </ScrollView>
         </View>
     );
@@ -67,15 +116,49 @@ const styles = StyleSheet.create({
            marginTop:27,
 
        },
-       textc:{
+       textC:{
            fontSize:25,
            fontWeight:"500",
            color:theme.grey,
        },
-       textm:{
+       textM:{
            fontSize:25,
            fontWeight:"500",
            color:theme.mainC,
+       },
+       line:{
+            borderTopWidth:2,
+            borderColor:theme.grey,
+            marginTop:10,
+            width: '100%',
+       },
+       option1:{
+            flexDirection:"row",
+            paddingTop: wp(14),
+       },
+       opText:{
+            flex:1,
+            fontSize: wp(4),
+            color:"black",
+       },
+       stText:{
+            fontSize: wp(3.6),
+            color:"grey",
+            justifyContent: 'flex-end',
+            marginRight: wp(-43),
+       },
+       opSwitch:{
+            flex:1,
+       },
+       btn: {
+            flex: 1,
+            width: '100%',
+            height: hp(5),
+            borderRadius: 10,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: theme.mainC,
+            marginTop: wp(16),
        },
 });
 
