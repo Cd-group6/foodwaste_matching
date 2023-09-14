8549449236eacc9b8f317b2ba0f8337275ba1caf
LKR <lkr9902@naver.com>
2023-09-14 17:20:14 +0900

    branch

--- client/src/auth/Login.js
+++ client/src/auth/Login.js
@@ -1,4 +1,4 @@
-import {
+{/*import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
@@ -129,4 +129,96 @@ const styles = StyleSheet.create({
 });
 
 
-export  default Login
+export  default Login*/}
+
+import * as React from 'react';
+import { useState,useEffect} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    ScrollView,
+    StatusBar,
+    Image,
+    Button,
+    TouchableOpacity,
+    ActivityIndicator,
+    } from 'react-native';
+
+import {
+    widthPercentageToDP as wp,
+    heightPercentageToDP as hp,
+    } from 'react-native-responsive-screen';
+
+import {theme} from "../colors.js";
+import Logo from '../assets/images/Logo.png';
+import Simg from '../assets/images/StartImg.png';
+
+const Login = ({navigation}) => {
+
+    return (
+        <ScrollView style={styles.container}>
+            <StatusBar style="auto" />
+            <View style={styles.logoCon}>
+                <Image style={styles.logo} source={Logo} />
+                <Text style={styles.expl}>
+                    실시간 매칭 음식물 쓰레기 처리 공유 플랫폼
+                </Text>
+                <Image style={styles.simg} source={Simg} />
+                <View style={styles.btnArea}>
+                    <TouchableOpacity style={styles.btn}
+                        onPress={() => navigation.navigate('MainTab')}>
+                        <Text style={styles.btnText}>카카오로 로그인</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </ScrollView>
+    );
+};
+
+const styles = StyleSheet.create({
+       container: {
+          backgroundColor: theme.bg,
+          paddingHorizontal:20,
+       },
+       logoCon:{
+          alignItems:"center",
+       },
+       logo: {
+          marginTop: 30,
+          width:350,
+          resizeMode: 'contain',
+       },
+       expl: {
+          marginTop:10,
+       },
+       simg: {
+          marginTop:-250,
+          width:300,
+          resizeMode: 'contain',
+       },
+       btnArea: {
+          marginTop:-260,
+          width:200,
+       },
+       btn: {
+         width: 200,
+         height:40,
+         borderRadius: 5,
+         justifyContent: 'center',
+         alignItems: 'center',
+         backgroundColor: "#fef01b",
+       },
+       btnText: {
+         color: 'black',
+         justifyContent: 'center',
+         alignItems: 'center',
+       },
+       activityIndicator: {
+         alignItems: 'center',
+         height: 80,
+         marginTop:-200,
+       },
+   });
+
+export default Login
