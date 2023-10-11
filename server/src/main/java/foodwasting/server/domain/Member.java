ed11b49fff8ff7bcbcd762a465099299e39f2ebd
Jung-kr <jungwy98@gmail.com>
2023-10-11 16:56:00 +0900

    feat : member & memberRepository 개발

---
+++ server/src/main/java/foodwasting/server/domain/Member.java
@@ -0,0 +1,20 @@
+package foodwasting.server.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String location;
+
+
+}
