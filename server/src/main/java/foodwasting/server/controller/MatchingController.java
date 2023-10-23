e09e1a57dd23586bb7a7691705a2d5880cb42e50
sincheol <ohmong0803@gmail.com>
2023-10-23 17:19:28 +0900

    test finished

--- server/src/main/java/foodwasting/server/controller/MatchingController.java
+++ server/src/main/java/foodwasting/server/controller/MatchingController.java
@@ -1,20 +1,17 @@
 package foodwasting.server.controller;
 
-import foodwasting.server.domain.Member;
 import foodwasting.server.dto.CreateMatchingRequest;
 import foodwasting.server.dto.CreateMatchingResponse;
-import foodwasting.server.service.KDTreeService;
-import foodwasting.server.service.MatchingService;
-import foodwasting.server.service.MemberService;
-import foodwasting.server.service.NodeService;
+import foodwasting.server.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,25 +21,41 @@ public class MatchingController {
     private final KDTreeService kdTreeService;
     private NodeService root = null;
 
+
     @PostMapping("/api/matching")
     public CreateMatchingResponse saveMatching(@RequestBody CreateMatchingRequest request) {
+        Logger logger = LoggerFactory.getLogger(MatchingController.class);
 
         Long matchingId = matchingService.matching(request);
 
-//        long test[] = {129L,384L};
-//        long test2[] = {132L,314L};
-//
-//
-//        System.out.println("test2 = " + test2.getClass().getName());
-//
-//        if (request.getTrashOwn()) {
-//            kdTreeService.insert(root, test);
-//        } else {
-//            kdTreeService.nearest(this.root, test2);
-//        }
+        Long[][] axes = {{3l, 6l}, {17l, 15l}, {13l, 15l}, {6l, 12l},
+                {9l, 1l}, {2l, 7l}, {10l, 19l}};
+        Long[] uid = {1l, 2l, 3l, 4l, 5l, 6l, 7l};
+
+        for (int i = 0; i < 7; i++) {
+            root = kdTreeService.insert(root, axes[i], uid[i]);
+        }
+
+        Long[] u1 = {10l, 11l};
+        Long uid1 = 1l;
+        UsrNodeService user1 = new UsrNodeService(uid1, u1);
+
+        Long[] u2 = {8l, 9l};
+        Long uid2 = 3l;
+        UsrNodeService user2 = new UsrNodeService(uid2, u2);
 
 
+        NodeService best1 = kdTreeService.nearest(root, user1.getAxes());
+        ArrayList<UsrNodeService> result = kdTreeService.findGroup(root, best1, user1);
 
+        NodeService best2 = kdTreeService.nearest(root, user2.getAxes());
+        result = kdTreeService.findGroup(root, best2, user2);
+
+        if (result != null) {
+            logger.info(String.valueOf(result.get(0).getUId()));
+            logger.info(String.valueOf(result.get(1).getUId()));
+            logger.info(String.valueOf(best1.getUId()));
+        }
 
         return new CreateMatchingResponse(matchingId);
     }
