975277d0267ab4c8d132ae2cb1692faa4b3da24d
sincheol <ohmong0803@gmail.com>
2023-10-25 22:12:45 +0900

    Long to double

--- server/src/main/java/foodwasting/server/service/UsrNodeService.java
+++ server/src/main/java/foodwasting/server/service/UsrNodeService.java
@@ -8,15 +8,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Getter
-@RequiredArgsConstructor
+
 public class UsrNodeService {
     Long uId;
-    Long[] axes;
+    double[] axes;
 
-    public UsrNodeService(Long uId, Long[] arr) {
+
+    protected UsrNodeService(){
+    }
+    public UsrNodeService(Long uId, double[] arr) {
         this.uId = uId;
         this.axes = arr;
     }
+
 
     public Long getUId() {
         return this.uId;
